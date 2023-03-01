67f3c4624fe42db2be071ceb59001d4c83fad236
Vale Sarro <valeria.sarro@rootstrap.com>
2023-03-01 17:08:02 -0300

    WIP create sign up background and add tailwind

---
+++ src/components/SignUpForm/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUpForm = () => {
+  return (
+    <div className='w-[22.5rem] h-[34.75rem]'>
+      <p>Holis</p>
+    </div>
+  );
+};
+
+export default SignUpForm;
---
+++ src/pages/SignUp/signUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignUpForm from '../../components/SignUpForm';
+import styles from './index.css';
+
+const SignUp = () => {
+  return (
+    <div className='background'>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUp;
