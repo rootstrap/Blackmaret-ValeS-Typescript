7d7eafb12aa8eb401526032e22e5aa89b4a06fec
Vale Sarro <valeria.sarro@rootstrap.com>
2023-03-07 16:35:20 -0300

    Remove old comment

--- src/components/shared/InputFields/index.tsx
+++ src/components/shared/InputFields/index.tsx
@@ -26,29 +26,3 @@ const InputField = ({ variant, text, required, placeholder }: InputProps) => {
 };
 
 export default InputField;
-
-// <InputField ofType='default' label='' required enabled error='false' />
-
-/*
-import styles from "./index.module.scss";
-import classnames from "classnames";
-
-const Button = ({ name, text, type, variant = "default", onClick }) => {
-  const variants = classnames({
-    [styles.smallButton]: variant === "small",
-    [styles.smallLinkText]: variant === "smallLink",
-    [styles.mediumButton]: variant === "medium",
-    [styles.largeButton]: variant === "default",
-    [styles.mediumLinkText]: variant === "mediumLink",
-    [styles.mediumLinkTextBlue]: variant === "mediumLinkBlue",
-  });
-  return (
-    <button name={name} className={variants} type={type} onClick={onClick}>
-      {text}
-    </button>
-  );
-};
-
-export default Button;
-
-*/
