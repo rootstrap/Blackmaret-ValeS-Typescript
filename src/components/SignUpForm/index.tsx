bfa5d461063aa83055e269090d71354c68fd1544
Vale Sarro <valeria.sarro@rootstrap.com>
2023-03-07 16:35:41 -0300

    Edit component styles

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -2,7 +2,7 @@ import InputField from '../shared/InputFields';
 
 const SignUpForm = () => {
   return (
-    <div className='flex-col box-content h-[34.75rem] w-[22.5rem] bg-indigo border-4m flex bg-white rounded-lg mt-[4.188rem] ml-[7.438rem]'>
+    <div className='flex-col box-content items-center h-[34.75rem] w-[22.5rem] bg-indigo border-4m flex bg-white rounded-lg mt-[4.188rem] ml-[7.438rem]'>
       <div> Black Market </div>
       <InputField text='Email' placeholder='Type your email' variant='simple' required={false} />
       <InputField
