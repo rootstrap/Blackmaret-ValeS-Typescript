c259381dd26c96d2bc03dc2b42b2dc27d7e11e33
Vale Sarro <valeria.sarro@rootstrap.com>
2023-03-08 12:06:19 -0300

    Add blackmarket logo

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,9 +1,10 @@
+import Logo from '../shared/Icons/logo';
 import InputField from '../shared/InputFields';
 
 const SignUpForm = () => {
   return (
     <div className='flex-col box-content items-center h-[34.75rem] w-[22.5rem] bg-indigo border-4m flex bg-white rounded-lg mt-16 ml-[7.438rem]'>
-      <div> Black Market </div>
+      <Logo className='h-[1.875rem] w-[10.813rem] mt-12' />
       <InputField text='Email' placeholder='Type your email' variant='simple' required={false} />
       <InputField
         text='Full Name'
