a1c5c5868cd337792d1df69fd511f04e5379e105
Vale Sarro <valeria.sarro@rootstrap.com>
2023-04-03 15:25:42 -0300

    WIP sign up

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -2,28 +2,68 @@ import ButtonLarge, { ButtonLargeVariants } from 'components/shared/ButtonsLarge
 import Logo from 'components/shared/Icons/Logo';
 import InputField, { InputVariants } from 'components/shared/InputFields';
 import Link from 'components/shared/Links/links';
+import { ChangeEvent, useState } from 'react';
+import { usePostRegistrationMutation } from 'services/blackMarketApi';
 
 const SignUpForm = () => {
+  const [registerRequest, { isSuccess: registerSuccess }] = usePostRegistrationMutation();
+
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    confPassword: '',
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  async function createUser(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const body = {
+      user: {
+        email: user.email,
+        password: user.password,
+        password_confirmation: user.password,
+        // password = password confirmation due to inconsistency between UI design and API
+      },
+    };
+    registerRequest(body);
+  }
+  if (registerSuccess) {
+    return <p>Register successful</p>;
+  }
   return (
     <div className='flex-col box-content items-center h-[34.7rem] w-[22.5rem] bg-indigo border-4m flex bg-white rounded-lg mt-16 ml-28'>
-      <Logo className='h-7 w-44 mt-12 mb-9' />
-      <InputField
-        text='Email'
-        placeholder='Type your email'
-        variant={InputVariants.Simple}
-        required={true}
-      />
-      <InputField
-        text='Full Name'
-        placeholder='Type your full name'
-        variant={InputVariants.Simple}
-      />
-      <InputField text='Password' placeholder='Type your password' variant={InputVariants.Simple} />
-      <div className='mt-3'>
-        <ButtonLarge variant={ButtonLargeVariants.Primary} disabled>
-          Sign Up
-        </ButtonLarge>
-      </div>
+      <form onSubmit={createUser}>
+        <Logo className='h-7 w-44 mt-12 mb-9' />
+        <InputField
+          text='Email'
+          placeholder='Type your email'
+          variant={InputVariants.Simple}
+          required={true}
+          value={user.email}
+          onChange={handleChange}
+        />
+        <InputField
+          text='Full Name'
+          placeholder='Type your full name'
+          variant={InputVariants.Simple}
+        />
+        <InputField
+          text='Password'
+          placeholder='Type your password'
+          variant={InputVariants.Simple}
+          value={user.password}
+          onChange={handleChange}
+        />
+        <div className='mt-3'>
+          <ButtonLarge variant={ButtonLargeVariants.Primary} disabled>
+            Sign Up
+          </ButtonLarge>
+        </div>
+      </form>
       <div className='flex flex-col items-center'>
         <div>
           <p className='text-center ml-8 mr-8 mt-5 mb-5'>
--- src/services/blackMarketApi.ts
+++ src/services/blackMarketApi.ts
@@ -2,19 +2,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 import { prepareHeaders } from 'utils/serviceUtils';
 
 export const blackMarketApi = createApi({
-  reducerPath: 'targetApi',
+  reducerPath: 'blackMarketAPI',
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.REACT_APP_BASE_URL,
     prepareHeaders,
   }),
   endpoints: (builder) => ({
-    postSignUp: builder.mutation({
-      query: (body) => ({ url: '/dj-rest-auth/login/', method: 'POST', body }),
+    postRegistration: builder.mutation({
+      query: (body) => ({ url: '/dj-rest-auth/registration/', method: 'POST', body }),
     }),
     deleteLogOut: builder.mutation({
-      query: (body) => ({ url: '/users/sign_out', method: 'DELETE', body }),
+      query: (body) => ({ url: '/dj-rest-auth/logout/', method: 'DELETE', body }),
     }),
   }),
 });
 
-export const { usePostSignUpMutation, useDeleteLogOutMutation } = blackMarketApi;
+export const { usePostRegistrationMutation, useDeleteLogOutMutation } = blackMarketApi;
