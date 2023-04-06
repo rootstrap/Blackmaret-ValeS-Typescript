868ab3fdcf89362263db1e353b91169989467e03
Vale Sarro <valeria.sarro@rootstrap.com>
2023-04-06 14:29:08 -0300

    Finish sign up

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -2,7 +2,7 @@ import ButtonLarge, { ButtonLargeVariants } from 'components/shared/ButtonsLarge
 import Logo from 'components/shared/Icons/Logo';
 import InputField, { InputVariants } from 'components/shared/InputFields';
 import Link from 'components/shared/Links/links';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, useEffect } from 'react';
 import { usePostRegistrationMutation } from 'services/blackMarketApi';
 
 const SignUpForm = () => {
@@ -14,30 +14,35 @@ const SignUpForm = () => {
     confPassword: '',
   });
 
+  const [formFilled, setFormFilled] = useState(false);
+
+  useEffect(() => {
+    setFormFilled(user.email !== '' && user.password !== '' && user.confPassword !== '');
+  }, [user]);
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setUser({ ...user, [name]: value });
-    console.log(user);
   };
 
   async function createUser(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const body = {
-      user: {
-        email: user.email,
-        password1: user.password,
-        password2: user.confPassword,
-      },
+      email: user.email,
+      password1: user.password,
+      password2: user.confPassword,
     };
     registerRequest(body);
   }
+
   if (registerSuccess) {
     return <p>Register successful</p>;
   }
+
   return (
-    <div className='flex-col box-content items-center h-[34.7rem] w-[22.5rem] bg-indigo border-4m flex bg-white rounded-lg mt-16 ml-28'>
+    <div className='mt-16 ml-28 box-content flex h-[34.7rem] w-[22.5rem] flex-col items-center rounded-lg bg-white'>
       <form onSubmit={createUser}>
-        <Logo className='h-7 w-44 mt-12 mb-9' />
+        <Logo className='mt-12 mb-9 h-7 w-44' />
         <InputField
           text='Email'
           placeholder='Type your email'
@@ -49,28 +54,30 @@ const SignUpForm = () => {
         <InputField
           text='Password'
           placeholder='Type your password'
-          variant={InputVariants.Simple}
+          variant={InputVariants.Password}
           value={user.password}
           onChange={handleChange}
+          type='password'
           name='password'
         />
         <InputField
           text='Confirm Password'
           placeholder='Repeat your password'
-          variant={InputVariants.Simple}
+          variant={InputVariants.Password}
           value={user.confPassword}
           onChange={handleChange}
+          type='password'
           name='confPassword'
         />
         <div className='mt-3'>
-          <ButtonLarge variant={ButtonLargeVariants.Primary} disabled>
+          <ButtonLarge variant={ButtonLargeVariants.Primary} disabled={!formFilled} type='submit'>
             Sign Up
           </ButtonLarge>
         </div>
       </form>
       <div className='flex flex-col items-center'>
         <div>
-          <p className='text-center ml-8 mr-8 mt-5 mb-5'>
+          <p className='mx-8 my-5 text-center'>
             By signing up, you accept the {<Link url='asd' text='Data Policy' />} and the{' '}
             {<Link url='asd' text='Cookies Policy' />}
           </p>
