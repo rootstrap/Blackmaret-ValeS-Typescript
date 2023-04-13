769820da81977b9fe6a83838614c58a08116f71d
Vale Sarro <valeria.sarro@rootstrap.com>
2023-04-13 12:10:44 -0300

    WIP fix GithubActions error

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,12 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import type { Config } from '@jest/types';
-import path from 'path';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  resolver: 'jest-resolver-enhanced',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '^components/(.*)$': '<rootDir>/src/components/$1',
   },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
-  roots: [path.resolve(__dirname, 'src')],
 };
 
 export default config;
