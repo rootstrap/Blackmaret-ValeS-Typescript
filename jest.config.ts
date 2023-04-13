5a1ba2064922a5dc2e5949f6222eab3ce4bf523d
Vale Sarro <valeria.sarro@rootstrap.com>
2023-04-13 12:00:44 -0300

    set path on jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from '@jest/types';
+import path from 'path';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
@@ -12,6 +13,7 @@ const config: Config.InitialOptions = {
       tsconfig: 'tsconfig.json',
     },
   },
+  roots: [path.resolve(__dirname, 'src')],
 };
 
 export default config;
