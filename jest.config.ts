b2642a80352619841f276116ed195b1b6bc46442
Vale Sarro <valeria.sarro@rootstrap.com>
2023-04-13 12:04:40 -0300

    WIP trying to fix github actions issue

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import type { Config } from '@jest/types';
 import path from 'path';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  resolver: 'jest-resolver-enhanced',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   moduleNameMapper: {
     '^components/(.*)$': '<rootDir>/src/components/$1',
