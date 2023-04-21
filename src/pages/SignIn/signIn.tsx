307ce1e1461e452d17af1ddf0f5b65bc613be494
Vale Sarro <valeria.sarro@rootstrap.com>
2023-04-21 16:23:28 -0300

    Create sign in page

---
+++ src/pages/SignIn/signIn.tsx
@@ -0,0 +1,11 @@
+import SignInForm from 'components/SignInForm';
+
+const SignIn = () => {
+  return (
+    <div className='absolute m-0 h-screen w-full bg-[url("/src/assets/background.jpg")] bg-cover p-0'>
+      <SignInForm />
+    </div>
+  );
+};
+
+export default SignIn;
