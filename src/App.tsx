a0ea037932b1b4dab8951af092a7ac00d07b9a92
Vale Sarro <valeria.sarro@rootstrap.com>
2023-04-21 16:22:31 -0300

    Update app.tsx to use react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,63 @@
-import SignUp from './pages/SignUp/signUp';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { AuthProvider } from './AuthContext';
+import { SIGNUP, SIGNIN, DASHBOARD } from './routes';
+import SignIn from 'pages/SignIn/signIn';
+import SignUp from 'pages/SignUp/signUp';
+import Dashboard from 'pages/Dashboard/dashboard';
+import PublicLayout from './layouts/PublicLayout';
+import PrivateLayout from './layouts/PrivateLayout';
+import { setCredentials } from 'store/auth.reducer';
+import { RootState } from 'store';
 
-function App() {
-  return <SignUp />;
-}
+const App: React.FC = () => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('user');
+    if (loggedInUser) {
+      dispatch(setCredentials(JSON.parse(loggedInUser)));
+    }
+  }, [dispatch]);
+
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={SIGNUP}
+            element={
+              <PublicLayout>
+                <SignUp />
+              </PublicLayout>
+            }
+          />
+          <Route
+            path={SIGNIN}
+            element={
+              <PublicLayout>
+                <SignIn />
+              </PublicLayout>
+            }
+          />
+          <Route
+            path={DASHBOARD}
+            element={
+              user ? (
+                <PrivateLayout>
+                  <Dashboard />
+                </PrivateLayout>
+              ) : (
+                <Navigate to={SIGNIN} replace />
+              )
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
 
 export default App;
