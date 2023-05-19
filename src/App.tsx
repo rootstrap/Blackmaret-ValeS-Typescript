c1715c4796bceb73aaf96bb3a56aa420a990fb70
ValesarroRS <valeria.sarro@rootstrap.com>
2023-05-19 18:11:07 -0300

    Add product list route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { AuthProvider } from './AuthContext';
-import { SIGNUP, SIGNIN, DASHBOARD } from './routes';
+import { SIGNUP, SIGNIN, DASHBOARD, PRODUCTLIST } from './routes';
 import SignIn from 'pages/SignIn/signIn';
 import SignUp from 'pages/SignUp/signUp';
 import Dashboard from 'pages/Dashboard/dashboard';
@@ -10,6 +10,7 @@ import PublicLayout from './layouts/PublicLayout';
 import PrivateLayout from './layouts/PrivateLayout';
 import { setCredentials } from 'store/auth.reducer';
 import { RootState } from 'store';
+import ProductList from 'pages/ProductsPage/productspage';
 
 const App: React.FC = () => {
   const { user } = useSelector((state: RootState) => state.auth);
@@ -48,6 +49,18 @@ const App: React.FC = () => {
               user ? (
                 <PrivateLayout>
                   <Dashboard />
+                </PrivateLayout>
+              ) : (
+                <Navigate to={SIGNIN} replace />
+              )
+            }
+          />
+          <Route
+            path={PRODUCTLIST}
+            element={
+              user ? (
+                <PrivateLayout>
+                  <ProductList />
                 </PrivateLayout>
               ) : (
                 <Navigate to={SIGNIN} replace />
