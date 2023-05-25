d2dea52ec8786a8ab0982a5880b53f9570bf6629
ValesarroRS <valeria.sarro@rootstrap.com>
2023-05-25 16:10:08 -0300

    Fix sign up and sign in styling

--- src/components/SignInForm/index.tsx
+++ src/components/SignInForm/index.tsx
@@ -3,8 +3,8 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Button, { ButtonSize, ButtonVariants } from 'components/shared/Buttons';
 import Logo from 'components/shared/Icons/Logo';
-import InputField, { InputVariants } from 'components/shared/InputFields';
 import Link from 'components/shared/Links';
+import InputField, { InputVariants } from 'components/shared/InputFields';
 import { useLogInMutation } from 'services/blackMarketApi';
 import Dashboard from 'pages/Dashboard/dashboard';
 import { SIGNUP } from 'routes';
@@ -39,7 +39,7 @@ const SignInForm = () => {
         dispatch(setCredentials(response.data));
         localStorage.setItem('auth', JSON.stringify(response.data));
         if (refreshToken !== null) {
-          localStorage.setItem('refresh_token', refreshToken); // save the refresh token to local storage
+          localStorage.setItem('refresh_token', refreshToken);
         }
       }
     } catch (error) {
@@ -55,40 +55,44 @@ const SignInForm = () => {
       <div className='mt-16 ml-28 box-content flex h-[26.5rem] w-[22.5rem] flex-col items-center rounded-lg bg-white'>
         <form onSubmit={authenticateUser}>
           <Logo className='ml-14 mt-12 mb-9 h-7 w-44' />
-          <InputField
-            text='Email'
-            placeholder='Type your email'
-            variant={InputVariants.Simple}
-            value={user.email}
-            onChange={handleChange}
-            name='email'
-          />
-          <InputField
-            text='Password'
-            placeholder='Type your password'
-            variant={InputVariants.Password}
-            value={user.password}
-            onChange={handleChange}
-            type='password'
-            name='password'
-          />
-          <div className='mt-3'>
-            <Button variant={ButtonVariants.Primary} size={ButtonSize.Large} type='submit'>
-              Log in
-            </Button>
+          <div className='h-full w-72'>
+            <InputField
+              text='Email'
+              placeholder='Type your email'
+              variant={InputVariants.Simple}
+              value={user.email}
+              onChange={handleChange}
+              name='email'
+            />
+            <InputField
+              text='Password'
+              placeholder='Type your password'
+              variant={InputVariants.Password}
+              value={user.password}
+              onChange={handleChange}
+              type='password'
+              name='password'
+            />
+            <div className='mt-3'>
+              <Button variant={ButtonVariants.Primary} size={ButtonSize.Large} type='submit'>
+                Log in
+              </Button>
+            </div>
           </div>
         </form>
         <p className='m-8 text-center'>{<Link url='asd' text='I forgot my password.' />}</p>
       </div>
       <div className='mt-5 ml-28 box-content flex h-32 w-[22.5rem] flex-col items-center rounded-lg bg-white'>
         <p className='mt-6 mb-4'> Don&apos;t have an account?</p>
-        <Button
-          variant={ButtonVariants.Outline}
-          size={ButtonSize.Large}
-          onClick={() => navigate(SIGNUP)}
-        >
-          Sign up
-        </Button>
+        <div className='h-full w-72'>
+          <Button
+            variant={ButtonVariants.Outline}
+            size={ButtonSize.Large}
+            onClick={() => navigate(SIGNUP)}
+          >
+            Sign up
+          </Button>
+        </div>
       </div>
     </>
   );
